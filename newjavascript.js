9224dfdd7e117a2e7cbf7087dd630bfd67735fc4
GoralDamian96 <49920701+GoralDamian96@users.noreply.github.com>
2019-05-27 22:51:19 +0200

    s

--- newjavascript.js
+++ newjavascript.js
@@ -747,56 +747,7 @@ putcanvasImg();
       points.length = 0;
     };
 }
-function drawCostam11()
-{
-    
-    thick.style.border="none";
-    fur.style.border="none";
-    pen_wid.style.border="none";
-    penmul.style.border="none";
-    brush.style.border="none";
-    pennor.style.border="none";
-    rysone.style.border="none";
-    rystwo.style.border="none";
-    rysthree.style.border="none";
-    rysfour.style.border="none";
-    rysfife.style.border="2px solid red";
-    penClick();
-    
-    
-    
-    var rect=canvas.getBoundingClientRect();
-    var el = document.getElementById('canvas');
-    ctx = el.getContext('2d');
-    var isDrawing;
 
-    var isDrawing, points = [ ];
-
-    el.onmousedown = function(e) {
-        getcanvasImg(); 
-      isDrawing = true;
-      points.push({ x: e.clientX-rect.left, y: e.clientY-rect.top });
-    };
-
-    el.onmousemove = function(e) {
-      if (!isDrawing) return;
-putcanvasImg();
-      
-      points.push({ x: e.clientX-rect.left, y: e.clientY-rect.top });
-
-      ctx.beginPath();
-      ctx.moveTo(points[0].x, points[0].y);
-      for (var i = 1; i < points.length; i++) {
-        ctx.lineTo(points[i].x, points[i].y);
-      }
-      ctx.stroke();
-    };
-
-    el.onmouseup = function() {
-      isDrawing = false;
-      points.length = 0;
-    };
-}
 
 
 
